dcb8931f7948ab7b588fe6af8fcd756b314c0b66
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-20 22:27:31 +0530

    source files index.js, app.js of react course 2020

---
+++ React_JS__Codedamn/React_Crash_Course_2020/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import logo from './logo.png'
+
+export default function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} alt="logo" />
+                <p>its cool</p>
+            </header>
+        </div>
+    )
+}
