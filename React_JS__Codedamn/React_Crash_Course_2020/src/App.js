f77398489c659c58f148019b405c3cf9071d380d
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-22 17:26:35 +0530

    App component

--- React_JS__Codedamn/React_Crash_Course_2020/src/App.js
+++ React_JS__Codedamn/React_Crash_Course_2020/src/App.js
@@ -1,13 +1,22 @@
 import React from "react"
-import logo from './logo.png'
 
-export default function App() {
+
+//this module discuss basic ideas of functional components, jsx types
+
+function  IAmAHigh() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} alt="logo" />
-                <p>its cool</p>
-            </header>
-        </div>
-    )
+      <div id="IAmAHigh">
+        <h1>Hello World {/* null, numbers, HTML, strings, arrays */} </h1>
+        <h1>Hello World Number {2 + 2}</h1>
+        <h1>Hello World {"String"}</h1>
+        <h1>Hello World Array - {["[one] ", "[two]"]}</h1>
+        <h1>Hello World HTML element {<h6 style={{margin:"2px"}}>wow</h6>}</h1>
+      </div>
+    );
 }
+function App() {
+    return (
+        <IAmAHigh/>
+    );
+}
+export default App; 
