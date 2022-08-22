1b4d48ff839a22e7f7a0528343260dd10c68d33f
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-22 17:25:53 +0530

    source index file

--- React_JS__Codedamn/React_Crash_Course_2020/src/index.js
+++ React_JS__Codedamn/React_Crash_Course_2020/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App'
+// import MyComponent from "./MyComponent";
 
-ReactDOM.render(<App />, document.getElementById("root"), () => {
-    console.log( "hi" );
+
+ReactDOM.render(<App/>, document.getElementById("root"), () => {
+    console.log( "app div rendered inside root div" );
 });
 
 
-
+// ReactDOM.render(<MyComponent/>, document.getElementById("root"), () => {
+//     console.log( "my component div rendered inside root div" );
+// });
 
