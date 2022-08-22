300c269a79074941385274b1e8f543a56d1fb77d
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-22 17:03:02 +0530

    added state, setState in the component

---
+++ React_JS__Codedamn/React_Crash_Course_2020/src/MyComponent.js
@@ -0,0 +1,33 @@
+import React from "react";
+class MyComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964,
+    };
+  }
+
+  changeColor = () => {
+    this.setState({
+      brand: "Chegg",
+      color: "green",
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>
+          hello world from MyComponent class having color {this.state.color}
+        </h1>
+        <button type="button" onClick={this.changeColor}>
+          click me
+        </button>
+      </div>
+    );
+  }
+}
+export default MyComponent;
