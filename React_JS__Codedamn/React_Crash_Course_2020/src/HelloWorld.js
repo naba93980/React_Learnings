c81354af4c974444b8ea8b7a8d18a774a740c91a
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-23 23:57:07 +0530

    updated helloworld component

--- React_JS__Codedamn/React_Crash_Course_2020/src/HelloWorld.js
+++ React_JS__Codedamn/React_Crash_Course_2020/src/HelloWorld.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-function HelloWorld() {
+function InnerCompo() {
     return (
         <h1>Hello World</h1>
     );
+}
+
+function HelloWorld() {
+    return <InnerCompo />;
 }
 export default HelloWorld; 
