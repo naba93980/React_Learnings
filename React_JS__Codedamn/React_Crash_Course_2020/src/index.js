ca346f5ae1a232b8ac0a9648aec00a3b60782839
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-24 13:01:21 +0530

    rendered new component

--- React_JS__Codedamn/React_Crash_Course_2020/src/index.js
+++ React_JS__Codedamn/React_Crash_Course_2020/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import HelloWorld from './HelloWorld';
-import MyClassComponent from "./MyClassComponent";
+// import HelloWorld from './HelloWorld';
+// import MyClassComponent from "./MyClassComponent";
+// import JSX from './JSX'
+// import ConditionalRendering from "./ConditionalRendering";
+// import NestingExp from "./NestingExpression";
+import EmptyTag from "./EmptyTag";
 
 
-// Rendering HelloWorld functional component -
+// Rendering HelloWorld component -
 
 // ReactDOM.render(<HelloWorld />, document.getElementById("root"), () => {
 //   console.log("HelloWorld rendered inside root div");
@@ -12,9 +16,36 @@ import MyClassComponent from "./MyClassComponent";
 
 
 
-// Rendering MyClassComponent - 
+// Rendering MyClassComponent -
 
-ReactDOM.render(<MyClassComponent/>, document.getElementById("root"), () => {
-    console.log( "my component div rendered inside root div" );
+// ReactDOM.render(<MyClassComponent/>, document.getElementById("root"), () => {
+//     console.log( "MyClassComponent div rendered inside root div" );
+// });
+
+
+// Rendering JSX component
+
+// ReactDOM.render(<JSX/>, document.getElementById("root"), () => {
+//     console.log( "my jsx component div rendered inside root div" );
+// });
+
+
+// Conditional Rendering
+
+// ReactDOM.render(<ConditionalRendering/>, document.getElementById("root"), () => {
+//     console.log( "condition checked and rendered inside root div" );
+// });
+
+
+// Rendering Nesting Expressions
+
+// ReactDOM.render(<NestingExp/>, document.getElementById("root"), () => {
+//     console.log( "nested expressions in jsx rendered inside root div" );
+// });
+
+
+ReactDOM.render(<EmptyTag />, document.getElementById("root"), () => {
+    console.log( "nested expressions in jsx rendered inside root div" );
 });
+
 
