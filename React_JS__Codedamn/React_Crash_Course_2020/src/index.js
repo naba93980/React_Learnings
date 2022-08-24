6bf4105c12e6e923e871522dc1e23df5222c55c7
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-25 04:43:14 +0530

    updated

--- React_JS__Codedamn/React_Crash_Course_2020/src/index.js
+++ React_JS__Codedamn/React_Crash_Course_2020/src/index.js
@@ -7,11 +7,11 @@ import ReactDOM from "react-dom";
 // import NestingExp from "./NestingExpression";
 // import EmptyTag from "./EmptyTag";
 // import Event from "./ClickEvent";
-import UseState from "./UseState";
+// import UseState from "./UseState";
+import DirectDOMaccess from "./StateComponent"
 
 
 // Rendering HelloWorld component -
-
 // ReactDOM.render(<HelloWorld />, document.getElementById("root"), () => {
 //   console.log("HelloWorld rendered inside root div");
 // });
@@ -19,31 +19,28 @@ import UseState from "./UseState";
 
 
 // Rendering MyClassComponent -
-
 // ReactDOM.render(<MyClassComponent/>, document.getElementById("root"), () => {
 //     console.log( "MyClassComponent div rendered inside root div" );
 // });
 
 
 // Rendering JSX component
-
 // ReactDOM.render(<JSX/>, document.getElementById("root"), () => {
 //     console.log( "my jsx component div rendered inside root div" );
 // });
 
 
 // Conditional Rendering
-
 // ReactDOM.render(<ConditionalRendering/>, document.getElementById("root"), () => {
 //     console.log( "condition checked and rendered inside root div" );
 // });
 
 
 // Rendering Nesting Expressions
-
 // ReactDOM.render(<NestingExp/>, document.getElementById("root"), () => {
 //     console.log( "nested expressions in jsx rendered inside root div" );
 // });
+
 
 // Rendering EmptyTag element
 // ReactDOM.render(<EmptyTag />, document.getElementById("root"));
@@ -54,5 +51,10 @@ import UseState from "./UseState";
 
 
 // useState in a component
-ReactDOM.render(<UseState />, document.getElementById("root"));
+// ReactDOM.render(<UseState />, document.getElementById("root"));
+
+
+// Rendering a component directly accessing DOM
+ReactDOM.render(<DirectDOMaccess />, document.getElementById("root"));
+
 
