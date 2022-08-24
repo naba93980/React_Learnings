90769b57964f78178713ba77c5aa5c210aae46d1
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-24 13:00:11 +0530

    changed code to pass js object as val in style att

--- React_JS__Codedamn/React_Crash_Course_2020/src/JSX.js
+++ React_JS__Codedamn/React_Crash_Course_2020/src/JSX.js
@@ -3,14 +3,20 @@ import React from "react"
 //this module discuss basic ideas of functional components, jsx types
 
 function JSX() {
-    return (
-        <div id="jsx-types">
-        <h1>Hello World {/* null, numbers, HTML, strings, arrays */} </h1>
-        <h1>Hello World Number {2 + 2}</h1>
-        <h1>Hello World {"String"}</h1>
-        <h1>Hello World Array - {["[one] ", "[two]"]}</h1>
-        <h1>Hello World HTML element {<h6 style={{margin:"2px"}}>wow</h6>}</h1>
-      </div>
-    );
+
+  const cssProps = { margin: "2px" };
+  return (
+    <div id="jsx-types">
+      <h1>Hello World {/* null, numbers, HTML, strings, arrays */} </h1>
+      <h1>Hello World Number {2 + 2}</h1>
+      <h1>Hello World {"String"}</h1>
+      <h1>Hello World Array - {["[one] ", "[two]"]}</h1>
+      <h1>Hello World HTML element {<h6 style={cssProps}>wow</h6>}</h1> 
+    </div>
+  );
 }
-export default App; 
+export default JSX;
+
+
+// The style attribute accepts a JavaScript object with camelCased properties rather than a CSS string.
+// This is consistent with the DOM style JavaScript property, is more efficient, and prevents XSS security holes.
