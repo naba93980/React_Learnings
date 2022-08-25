e436867d33010db94be5d1b6a2f7aea470ea5c44
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-25 06:02:19 +0530

    confusion cleared, dom should not be accessed directly

--- React_JS__Codedamn/React_Crash_Course_2020/src/StateComponent.js
+++ React_JS__Codedamn/React_Crash_Course_2020/src/StateComponent.js
@@ -1,22 +1,27 @@
 import React from "react";
 
-export default function StateComponent() {
-    let counter = 0;
-    const change = (e) => {
-        counter++
-        console.log(counter);
-        document.getElementsByClassName('h1')[0].innerHTML = counter;
-    };
-    const reset = (e) => {
-        counter = 0;
-        console.log(counter);
-        document.querySelector('.h1').innerHTML = counter;
-    };
-    return (
-        <>
-            <h1 className="h1">{counter}</h1>
-            <button onClick={change}>click me</button>
-            <button onClick={reset}>reset me</button>
-        </>
-    );
+export default function DirectDOMaccess() {
+  let counter = 0;
+  var g = document.querySelector(".h1"); // returns null bcoz when this line is executed, DOM elements are not rendered on screen bcoz the return statement is executed after this line executed, that is why useRef() should be used.
+
+  const change = (e) => {
+    counter++;
+    console.log(counter);
+    document.getElementsByClassName("h1")[0].innerHTML = counter; //this works perfectly bcoz the DOM is rendered before the button is clicked
+  };
+  const reset = (e) => {
+    counter = 100;
+    console.log(counter);
+    // document.querySelector(".h1").innerHTML = counter;
+  };
+  return (
+    <>
+      <h1 className="h1">{counter}</h1>
+      <button onClick={change}>click me</button>
+      <button onClick={reset}>reset me</button>
+    </>
+  );
 }
+
+// manipulaying DOM directly here is problematic as it based on external variable counter and not on the state(bortoman obostha) of the element in DOM
+// when we use useState(), the DOM is updated based on the state value(bortoman obostha) of the element in the DOM.
