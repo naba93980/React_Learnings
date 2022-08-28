52cb1a76a1188463c8dee95a9ebc588a0cc686fc
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-29 02:31:54 +0530

    new components rendered

--- React_JS__Codedamn/React_Crash_Course_2020/src/index.js
+++ React_JS__Codedamn/React_Crash_Course_2020/src/index.js
@@ -3,58 +3,69 @@ import ReactDOM from "react-dom";
 // import HelloWorld from './HelloWorld';
 // import MyClassComponent from "./MyClassComponent";
 // import JSX from './JSX'
-// import ConditionalRendering from "./ConditionalRendering";
+// import TernaryConditionalRendering from "./TernaryConditionalRendering";
 // import NestingExp from "./NestingExpression";
 // import EmptyTag from "./EmptyTag";
 // import Event from "./ClickEvent";
 // import DirectDOMaccess from "./StateComponent"
-import UseState from "./UseState";
-
+// import UseState from "./UseState";
+// import ReactElementObject from "./ReactElementObject";
+// import Form from "./Forms"
+// import IfElseCondRend from "./IfElseCondRend";
+// import ShortCircuitCondRend from "./ShortCircuitCondRend";
+// import Greet from "./Props"
+// import Greet from "./ChildProp"
+import List from "./List";
 
 // Rendering HelloWorld component -
 // ReactDOM.render(<HelloWorld />, document.getElementById("root"), () => {
 //   console.log("HelloWorld rendered inside root div");
 // });
 
-
+// ReactDOM.render(React.createElement("p", {key:2}, "hello ", 5 + 6), document.getElementById("root"));
 
 // Rendering MyClassComponent -
 // ReactDOM.render(<MyClassComponent/>, document.getElementById("root"), () => {
 //     console.log( "MyClassComponent div rendered inside root div" );
 // });
 
-
 // Rendering JSX component
 // ReactDOM.render(<JSX/>, document.getElementById("root"), () => {
 //     console.log( "my jsx component div rendered inside root div" );
 // });
-
 
 // Conditional Rendering
 // ReactDOM.render(<ConditionalRendering/>, document.getElementById("root"), () => {
 //     console.log( "condition checked and rendered inside root div" );
 // });
 
-
 // Rendering Nesting Expressions
 // ReactDOM.render(<NestingExp/>, document.getElementById("root"), () => {
 //     console.log( "nested expressions in jsx rendered inside root div" );
 // });
 
-
 // Rendering EmptyTag element
 // ReactDOM.render(<EmptyTag />, document.getElementById("root"));
-
 
 // Rendering Event element
 // ReactDOM.render(<Event />, document.getElementById("root"));
 
-
 // useState in a component
 // ReactDOM.render(<UseState />, document.getElementById("root"));
-
 
 // Rendering a component directly accessing DOM
 // ReactDOM.render(<DirectDOMaccess />, document.getElementById("root"));
 
+// Rendering a jsx element (React object)
+// ReactDOM.render(<ReactElementObject />, document.getElementById('root'));
 
+// Rendering Forms
+// ReactDOM.render(<Form />, document.getElementById('root'));
+
+// ReactDOM.render(<IfElseCondRend />, document.getElementById('root'));
+
+// ReactDOM.render(<ShortCircuitCondRend />, document.getElementById('root'));
+
+// ReactDOM.render(<Greet />, document.getElementById('root'));
+
+ReactDOM.render(<List />, document.getElementById('root'));
