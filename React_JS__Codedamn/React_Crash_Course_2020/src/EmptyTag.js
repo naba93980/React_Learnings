ad3eb6aead3873c0c81ba657391c3067bf120ef2
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-08-30 23:11:44 +0530

    separated elements into components

--- React_JS__Codedamn/React_Crash_Course_2020/src/EmptyTag.js
+++ React_JS__Codedamn/React_Crash_Course_2020/src/EmptyTag.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+
+function Hello() {
+  return <h1>Hello</h1>;
+}
+
+function Bye() {
+  return <h1>Bye</h1>;
+}
 
 export function EmptyTag() {
-  return (
+  let r = (
     <>
-      <h1>hello</h1>
+      <Hello/>
       <h2>world</h2>
+      <button onClick={change}>click</button>
     </>
   );
+
+  const [ren, setRen] = useState(r);
+
+  function change() {
+    r = (
+      <>
+        <Bye/>
+        <h2>world</h2>
+        <button onClick={change}>click</button>
+      </>
+    );
+    console.log(r);
+    setRen(r);
+  }
+
+  return ren;
 }
 export default EmptyTag;
