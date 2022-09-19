fba3c9ec41e4a28486cde958a47dcd09746d8b92
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-20 03:20:21 +0530

    updated

--- React_JS__Codedamn/React_Crash_Course_2020/src/ChildProp.js
+++ React_JS__Codedamn/React_Crash_Course_2020/src/ChildProp.js
@@ -3,16 +3,19 @@ import React from "react";
 function Timing(props) {
   console.log(props);
   return (
-    <p>
+    <div>
           Good {props.time} {props.name} <br />
           {props.children}
-    </p>
+    </div>
   );
 }
 export default function Greet() {
   return (
     <Timing time="day" name="naba">
-      <p>hello guys</p>   { /* passed as children prop to the child compnent*/ }
+      {<p>hello guys</p>}   { /* passed as children prop to the child compnent*/ }
     </Timing>
   );
 }
+// The content passed to a component through props.children can include undefined, null, a Boolean,
+// a number, a string, a React element,  or an array of any of these types recursively.
+// It can also be a function returning one of these types.
