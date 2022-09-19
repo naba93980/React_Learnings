64df8388f03bda447ea02d22574ef38f7aeea9db
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-20 03:22:54 +0530

    renamed UseContext to Context

--- React_JS__Codedamn/React_Crash_Course_2020/src/PropsDrilling&Context/Context.js
+++ React_JS__Codedamn/React_Crash_Course_2020/src/PropsDrilling&Context/Context.js
@@ -2,7 +2,7 @@ import React, { useState} from "react";
 import DisplayCount from "./DisplayCount.js";
 import countContext from "./CountContext";
 
-export default function UseContext() {
+export default function Context() {
   const [count, setCount] = useState(0);
   return (
     <div>
