d254074abfac28ac8c3e79352891897f319e71a5
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-20 20:46:28 +0530

    updated with imports DisplayCount  component that uses useContext API

--- React_JS__Codedamn/React_Crash_Course_2020/src/PropsDrilling&Context/Context.js
+++ React_JS__Codedamn/React_Crash_Course_2020/src/PropsDrilling&Context/Context.js
@@ -1,5 +1,6 @@
 import React, { useState} from "react";
-import DisplayCount from "./DisplayCount.js";
+// import DisplayCount from "./DisplayCount.js";
+import DisplayCount from "./ContextApiDisplay";
 import countContext from "./CountContext";
 
 export default function Context() {
