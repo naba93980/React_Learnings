98a5b64c3e9ece5b94099286e5d1bdfa7b59e06a
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-20 20:47:04 +0530

    updated fn run by context.consumer component

--- React_JS__Codedamn/React_Crash_Course_2020/src/PropsDrilling&Context/Button.js
+++ React_JS__Codedamn/React_Crash_Course_2020/src/PropsDrilling&Context/Button.js
@@ -2,20 +2,21 @@ import React from "react";
 import countContext from "./CountContext";
 
 export default function Button() {
+  const fn = (value) => {
+    const updateCount = () => {
+      // const count = value.count;
+      value.setCount(value.count + 1);
+    };
+    return <button onClick={updateCount}>click me</button>;
+  }
   return (
     <div>
       <countContext.Consumer>
-        {(value) => {
-          const updateCount = () => {
-            const count = value.count;
-            value.setCount(count + 1);
-          };
-          return <button onClick={updateCount}>click me</button>;
-        }}
+        {fn}
       </countContext.Consumer>
     </div>
   );
-
+  
   //   return (
   //     <button onClick={()=>{props.setCount((count)=>count+1)}}>click me</button>
   //   )
