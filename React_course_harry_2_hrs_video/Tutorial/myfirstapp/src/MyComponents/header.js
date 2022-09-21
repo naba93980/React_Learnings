b08d8c1530ded6c6477a128bd79897d114f8a0d9
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-22 00:23:37 +0530

    added navbar component of  bootstrap in header component

--- React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/MyComponents/header.js
+++ React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/MyComponents/header.js
@@ -1,31 +1,59 @@
 import React from "react";
-import PropTypes from "prop-types";
-import "./header.css";
 export function Header(props) {
   return (
-    <nav className="navigationbar">
-      <span className="home">
-        <a href="https://www.google.com">TodoList</a>
-      </span>
-      <span className="about">
-        <a href="https://www.google.com">About</a>
-      </span>
-      <span className="todolist">
-        <a href="https://www.google.com">
-          {props.title} {props.val} {props.roll}
-        </a>
-      </span>
-    </nav>
-  );
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="https://www.google.com">
+            {props.title}
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="https://www.google.com"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://www.google.com">
+                  About
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
 }
-Header.propTypes = {
-  var: PropTypes.number,
-  title:
-    PropTypes.string.isRequired /* isRequired matlab ye props arguments ka sochke pass karna hi karna hai */,
-};
+
+export default Header;
 Header.defaultProps = {
-  title: "kuch daldo",
-  nam: "mera nam",
-  roll: 456,
-};
-// export default header
+  "title": "your_title",
+}
