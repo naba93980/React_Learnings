23cc6416ee92682cbddbec5ec75ef5dbf64de276
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-23 23:32:55 +0530

    implmented addTodo function and passed it to AddTodo component

--- React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/App.js
+++ React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useState } from "react";
+import { AddTodo } from "./MyComponents/addtodo.js"
 import { Header } from "./MyComponents/header.js";
 import { Todos } from "./MyComponents/todos.js";
-import footer from "./MyComponents/footer.js";
+import { Footer } from "./MyComponents/footer.js";
 
 function App() {
   
@@ -25,14 +26,20 @@ function App() {
   ];
   const [todos, setToDosArray] = useState(todosArray);
   const deleteTodo = (todoitem) => {
-    setToDosArray((todos) => todos.filter((ele) => ele != todoitem));
+    setToDosArray((todos) => todos.filter((ele) => ele !== todoitem));
+  }
+  const addTodo = (title, desc) => {
+    let sno = todos.length + 1;
+    setToDosArray([...todos, { sno, title, desc }]);
   }
   return (
-    <div className="container-fluid">
+    <div className="container-fluid p-0">
       <div className="row">
         <div className="col-12">
           <Header title="My Todos List" />
+          <AddTodo addTodo={addTodo} />
           <Todos todos={todos} deleteTodo={deleteTodo} />
+          <Footer />
         </div>
       </div>
     </div>
