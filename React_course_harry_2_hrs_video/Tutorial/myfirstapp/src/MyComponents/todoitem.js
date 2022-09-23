8f0dc2bb3828e940a1b77e92651de4257691bffb
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-23 11:45:04 +0530

    renders each todoitem, styling added

--- React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/MyComponents/todoitem.js
+++ React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/MyComponents/todoitem.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-export const Todoitem = (props) => {
+export function Todoitem(props) {
+  // const Delete = (e) => {
+  //   console.log(e.target.offsetParent);
+  //   e.target.offsetParent.hidden = true;
+  // }
+
+  const del = () => {
+    props.deleteTodo(props.item);
+  };
+
   return (
-      <div>
-          <div>
-            {props.todoarrelementobject.sno}<br />          
-            {props.todoarrelementobject.title}<br/>
-            {props.todoarrelementobject.description}<br/>
-            <button>delete</button>
-          </div>    
+    <>
+      <div className="col-auto">
+        {<h2>{props.item.sno}</h2>}
+        {<p>{props.item.title}</p>}
+        {<p>{props.item.description}</p>}
+        {
+          <button onClick={del} type="button" className="btn btn-danger">
+            Delete
+          </button>
+        }
       </div>
-      
-  )
+    </>
+  );
 }
