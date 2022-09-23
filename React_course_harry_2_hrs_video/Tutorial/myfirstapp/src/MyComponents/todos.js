423ac9f0df76611dc8899c23167b2ce5e78b7cb6
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-23 23:25:54 +0530

    added min height taken by todos component so that footer goes to bottom of the page

--- React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/MyComponents/todos.js
+++ React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/MyComponents/todos.js
@@ -2,10 +2,16 @@ import React from "react";
 import { Todoitem } from "./todoitem";
 export function Todos(props) {
   return (
-    <div className="row justify-content-around">
-      {props.todos.length===0?"no todos to display": props.todos.map((e, i) => {
-        return <Todoitem key={i} item={e} deleteTodo={props.deleteTodo} />;
-      })}
-    </div>
+    <div className="row justify-content-center p-5" style={
+      {
+        minHeight: "50vh"
+      }
+    }>
+      {
+        props.todos.length === 0 ? "no todos to display" : props.todos.map((e, i) => {
+          return <Todoitem key={i} item={e} deleteTodo={props.deleteTodo} />;
+        })
+      }
+    </div >
   );
 }
