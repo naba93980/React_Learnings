fce1a55b413194aaae719465bd5e824ccfbd03e4
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-23 11:43:02 +0530

    app component rendering header and todos component

--- React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/App.js
+++ React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/App.js
@@ -1,64 +1,41 @@
-// import logo from "./logo.svg";
-import "./App.css";
-import { Header } from "./MyComponents/header.js"
+import React from "react";
+import { useState } from "react";
+import { Header } from "./MyComponents/header.js";
 import { Todos } from "./MyComponents/todos.js";
-// import footer from "./MyComponents/footer.js";
-
-
+import footer from "./MyComponents/footer.js";
 
 function App() {
-  //below code is jsx
-  // let sirname = "modak";
-  let todosarray = [
+  
+  let todosArray = [
     {
       sno: 1,
       title: "go to market",
-      description : " 1yo need to go maret to get this done",
+      description: " 1yo need to go maret to get this done",
     },
     {
       sno: 2,
       title: "go to mall",
-      description : " 2yo need to go maret to get this done",
+      description: " 2yo need to go maret to get this done",
     },
     {
       sno: 3,
       title: "go to ghat",
-      description : " 3yo need to go maret to get this done",
+      description: " 3yo need to go maret to get this done",
     },
-  ]
-  console.log(todosarray)
+  ];
+  const [todos, setToDosArray] = useState(todosArray);
+  const deleteTodo = (todoitem) => {
+    setToDosArray((todos) => todos.filter((ele) => ele != todoitem));
+  }
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <div> {`nabajyoti ${sirname}`}</div>
-    //     <div> {2 + 20}</div>
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>Edit src/App.js and save to reload.</p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
-    //------------------
-    // <div className='App'>
-    //   <nav>
-    //     {"hello"}
-    //   </nav>
-    // </div>
-    <div id="app">
-      <Header val={45} />
-      {/* {Header({ title: "My Todo List", val: 78 })} avoid it */}
-      <Todos todosarrayprops={todosarray} />
-      {/* {todoitem()} */}
-      {/* {footer()} */}
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <Header title="My Todos List" />
+          <Todos todos={todos} deleteTodo={deleteTodo} />
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
