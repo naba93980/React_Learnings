ab3790ed81bec6669ec56ec38dfb64e00afbca5a
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-26 22:27:42 +0530

    updated min height of todos component and passed sno prop

--- React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/MyComponents/todos.js
+++ React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/MyComponents/todos.js
@@ -4,12 +4,12 @@ export function Todos(props) {
   return (
     <div className="row justify-content-center p-5" style={
       {
-        minHeight: "50vh"
+        minHeight: "90vh"
       }
     }>
       {
         props.todos.length === 0 ? "no todos to display" : props.todos.map((e, i) => {
-          return <Todoitem key={i} item={e} deleteTodo={props.deleteTodo} />;
+          return <Todoitem key={i} sno={i+1} item={e} deleteTodo={props.deleteTodo} />;
         })
       }
     </div >
