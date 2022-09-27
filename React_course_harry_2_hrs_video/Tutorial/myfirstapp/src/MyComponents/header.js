491e48b19a63fe80992ed44a58c5e9e1b2fa9588
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-27 18:05:40 +0530

    updated links for routing

--- React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/MyComponents/header.js
+++ React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/MyComponents/header.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 export function Header(props) {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="https://www.google.com">
+          <Link className="navbar-brand" to="/">
             {props.title}
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -21,18 +22,18 @@ export function Header(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a
+                <Link
                   className="nav-link active"
                   aria-current="page"
-                  href="https://www.google.com"
+                  to="home"
                 >
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="https://www.google.com">
+                <Link className="nav-link" to="about">
                   About
-                </a>
+                </Link>
               </li>
             </ul>
             <form className="d-flex">
