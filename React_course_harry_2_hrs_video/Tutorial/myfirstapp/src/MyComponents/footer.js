fe871eba411088cd87a6788ba94cdbbc814596b0
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-27 17:43:41 +0530

    updated margin of footer component

--- React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/MyComponents/footer.js
+++ React_course_harry_2_hrs_video/Tutorial/myfirstapp/src/MyComponents/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function Footer(){
   return (
-    <footer className='bg-dark text-light'>
+    <footer className='bg-dark text-light' style={{ marginTop: "100vh" }}>
       {<p className='text-center m-0'>Copyright &copy; mytodolist.com</p>}
     </footer>
   );
