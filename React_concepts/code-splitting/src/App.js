fa083509a0178680fdced818f11629877450e93c
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-12-20 23:04:02 +0530

    async loading react components

--- React_concepts/code-splitting/src/App.js
+++ React_concepts/code-splitting/src/App.js
@@ -2,10 +2,17 @@ import { useState } from "react";
 
 function App() {
   const [names, setNames] = useState(null);
+  const [MyDefaultComponent, setMyDefaultComponent] = useState(null);
+
   const onLoad = async () => {
     const names = (await import("./names" /* webpackChunkName: "names" */)).default;
-    const makeUpperCase =  (await import("./utilities" /* webpackChunkName: "utilities" */)).makeUpperCase;
+    
+    const makeUpperCase = (await import("./utilities" /* webpackChunkName: "utilities" */)).makeUpperCase;
     setNames(makeUpperCase(names));
+
+    const MyDefaultComponent = (await import("./MyDefaultComponent")).default;
+    // console.log(MyDefaultComponent());
+    setMyDefaultComponent(MyDefaultComponent);
   };
 
   return (
@@ -13,6 +20,7 @@ function App() {
       <p>Home</p>
       <button onClick={onLoad}>Load</button>
       <p>{JSON.stringify(names)}</p>
+      {MyDefaultComponent}
     </>
   );
 }
