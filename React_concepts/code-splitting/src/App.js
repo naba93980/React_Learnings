d4d62bd0e54498635cacbae44e1920689795155c
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-12-20 18:56:36 +0530

    async module loading using async await and custom naming of chunks

--- React_concepts/code-splitting/src/App.js
+++ React_concepts/code-splitting/src/App.js
@@ -2,20 +2,17 @@ import { useState } from "react";
 
 function App() {
   const [names, setNames] = useState(null);
-  const onLoad = () => {
-    import("./names")
-      .then(module => {
-        console.log(module);
-        import("./utilities").then(({ makeUpperCase }) =>
-          setNames(makeUpperCase(module.default))
-        );
-      });
-  }
+  const onLoad = async () => {
+    const names = (await import("./names" /* webpackChunkName: "names" */)).default;
+    const makeUpperCase =  (await import("./utilities" /* webpackChunkName: "utilities" */)).makeUpperCase;
+    setNames(makeUpperCase(names));
+  };
+
   return (
     <>
       <p>Home</p>
       <button onClick={onLoad}>Load</button>
-      <p>{ JSON.stringify(names)}</p>
+      <p>{JSON.stringify(names)}</p>
     </>
   );
 }
