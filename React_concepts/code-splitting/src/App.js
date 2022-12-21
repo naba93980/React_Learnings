2857490f62bd8fc5661e7da5ca9494a920ac964f
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-12-21 15:04:33 +0530

    Manually Wrapping Async Loaded React Component

--- React_concepts/code-splitting/src/App.js
+++ React_concepts/code-splitting/src/App.js
@@ -1,18 +1,44 @@
-import { useState } from "react";
+import  React,{ useState } from "react";
+
+function loadComponent(importFunc) {
+  return class WrappedComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        Component: null,
+      }
+    }
+    componentDidMount() {
+      console.log("inside didMount");
+      importFunc().then((mod) => {
+        this.setState({
+          Component: mod.default,
+        })
+      })
+    }
+    render() {
+      console.log("inside render");
+      return this.state.Component ? (
+        <this.state.Component {...this.props} />
+      ) : null;
+    }
+  };
+}
+
+const MyDefaultComponent = loadComponent(() =>
+  import("./MyDefaultComponent" /* webpackChunkName: "mydefaultcomponent" */)
+);
 
 function App() {
+  console.log("app");
+  console.log(MyDefaultComponent);
   const [names, setNames] = useState(null);
-  const [MyDefaultComponent, setMyDefaultComponent] = useState(null);
 
   const onLoad = async () => {
     const names = (await import("./names" /* webpackChunkName: "names" */)).default;
     
     const makeUpperCase = (await import("./utilities" /* webpackChunkName: "utilities" */)).makeUpperCase;
     setNames(makeUpperCase(names));
-
-    const MyDefaultComponent = (await import("./MyDefaultComponent")).default;
-    // console.log(MyDefaultComponent());
-    setMyDefaultComponent(MyDefaultComponent);
   };
 
   return (
@@ -20,7 +46,7 @@ function App() {
       <p>Home</p>
       <button onClick={onLoad}>Load</button>
       <p>{JSON.stringify(names)}</p>
-      {MyDefaultComponent}
+      {names && <MyDefaultComponent />}
     </>
   );
 }
