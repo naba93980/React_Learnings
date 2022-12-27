4cefd295e810a61f4a90c5695f05d334a9afad92
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-12-28 02:52:58 +0530

    server renders a page as per req url using StaticRouter and sends initial html file as string

--- React_concepts/ssr/server.js
+++ React_concepts/ssr/server.js
@@ -1,7 +1,38 @@
-const express = require('express');
-const app = express();
+import 'babel-polyfill';
+import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from "react-router-dom/server";
+import bodyParser from 'body-parser';
+import App from './src/App';
 
+const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(express.static('build'));
+
+app.get('*', (req, res) => {
+
+    const context = {};
+    const content = ReactDOMServer.renderToString(
+        <StaticRouter location={req.url} context={context}>
+            <App />
+        </StaticRouter>
+    );
+    const html = `
+        <html>
+            <head>
+            </head>
+            <body>
+                <div id="root">
+                    ${content}
+                </div>
+            </body>
+        </html >
+    `;
+    res.send(html);
+})
 
 app.listen(PORT, () => {
     console.log("app running");
