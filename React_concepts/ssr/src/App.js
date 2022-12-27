fada5b79e03f3f87dffc6fa5296bec1d0c264e4d
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-12-28 02:36:13 +0530

    added new about route

--- React_concepts/ssr/src/App.js
+++ React_concepts/ssr/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { Route, Router } from "react-router";
+import { Route, Routes } from "react-router";
 import Home from "./pages/HomePageComponent";
+import About from "./pages/AboutPageComponent";
 
 function App() {
-  return (   
-    <>
-      <Router>
-        <Route path="/" exact={true} render={Home} />
-      </Router>
-    </>
-  )
+  return (
+    <div>    
+          <Routes>
+            <Route path="/" exact={true} element={<Home />} />
+            <Route path="/about" exact={true} element={<About />} />
+          </Routes>
+    </div>
+  );
 }
 
 export default App;
