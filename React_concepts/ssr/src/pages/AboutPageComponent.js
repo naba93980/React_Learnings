b73b57ea2362e53bc2ffffbb81b03fd0a27d6ed8
NabajyotiModak <naba93981@gmail.com>
2022-12-30 02:42:02 +0530

    added some js in about page component

--- React_concepts/ssr/src/pages/AboutPageComponent.js
+++ React_concepts/ssr/src/pages/AboutPageComponent.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function AboutPageComponent() {
+    const [about, setAbout] = useState(" nabajyoti ");
+    const fixAbout = () => {
+        if (about === " nabajyoti ")
+            setAbout(" modak ");
+        else
+            setAbout(" nabajyoti ");
+    }
     return (
-        <div>AboutPageComponent</div>
+        <div>
+            <div onClick={fixAbout}>About{ about }PageComponent</div>
+        </div>
     )
 }
