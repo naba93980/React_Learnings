ad5afdb45be8998d89a4f3e4d5e916a0bbc58178
NabajyotiModak <naba93981@gmail.com>
2023-01-18 17:52:46 +0530

    Provider that provides the QueryClient to the app

--- React-Query/react-query-demo/src/App.js
+++ React-Query/react-query-demo/src/App.js
@@ -1,33 +1,41 @@
 import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home'
-import SuperHeroes from './components/SuperHeroes'
-import RQSuperHeroes from './components/RQSuperHeroes'
+import Home from './components/Home';
+import SuperHeroes from './components/SuperHeroes';
+import RQSuperHeroes from './components/RQSuperHeroes';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import './App.css'
 
+const queryClient = new QueryClient();
+
 function App() {
+
   return (
-    <div>
-      <BrowserRouter>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/super-heroes'>Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/super-heroes' element={<SuperHeroes />} />
-          <Route path='/rq-super-heroes' element={<RQSuperHeroes />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/super-heroes'>Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          </div>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/super-heroes' element={<SuperHeroes />} />
+            <Route path='/rq-super-heroes' element={<RQSuperHeroes />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    
   );
 }
 
