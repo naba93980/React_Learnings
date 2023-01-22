66a10c1f227da640fc4c6cd502b87d178888cfbf
NabajyotiModak <naba93981@gmail.com>
2023-01-22 18:50:56 +0530

    added initialData prop for a query

--- React-Query/query-types/src/hooks/useSuperHeroData.js
+++ React-Query/query-types/src/hooks/useSuperHeroData.js
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query'
+import { useQuery, useQueryClient } from 'react-query'
 import axios from 'axios'
 
 const fetchSuperHero = ({ queryKey }) => {
@@ -7,8 +7,20 @@ const fetchSuperHero = ({ queryKey }) => {
 }
 
 export const useSuperHeroData = (heroId) => {
+    const queryClient = useQueryClient();
     // return useQuery(['super-hero', heroId],()=> fetchSuperHero(heroId),{
     return useQuery(['super-hero', heroId], fetchSuperHero, {
-        refetchOnMount: true
+        refetchOnMount: true,
+        initialData: ()=> {
+            const hero = queryClient.getQueryData('super-heroes')?.data?.find(hero=>hero.id===parseInt(heroId))
+            if(hero){
+                return {
+                    data: hero
+                }
+            }
+            else{
+                return undefined
+            }
+        }
     })
 }
