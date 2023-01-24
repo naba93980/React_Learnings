64b9bbcef7eaff8a6934c1a21380e37426e956f1
NabajyotiModak <naba93981@gmail.com>
2023-01-24 22:50:23 +0530

    onSuccess of POSTing data, current query data is invalidated and data is fetched again

--- React-Query/mutations/src/hooks/useSuperHeroesData.js
+++ React-Query/mutations/src/hooks/useSuperHeroesData.js
@@ -1,4 +1,5 @@
 import { useQuery, useMutation } from 'react-query'
+import { useQueryClient } from 'react-query';
 import axios from 'axios'
 
 const fetchSuperHeroes = () => {
@@ -17,6 +18,11 @@ export const useSuperHeroesData = (onSuccess, onError) => {
 }
 
 export const useAddSuperHeroData = () => {
-    return useMutation(addSuperHero)
+    const queryClient = useQueryClient();
+    return useMutation(addSuperHero,{
+        onSuccess: ()=>{
+            queryClient.invalidateQueries('super-heroes');
+        }
+    })
 }
 
