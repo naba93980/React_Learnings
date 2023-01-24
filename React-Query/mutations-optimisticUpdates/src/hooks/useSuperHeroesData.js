3a220940dd0b480ea6b8cccf29e86499b8df9b20
NabajyotiModak <naba93981@gmail.com>
2023-01-25 00:38:57 +0530

    basic setup onMutate, onSettled, onError fns

---
+++ React-Query/mutations-optimisticUpdates/src/hooks/useSuperHeroesData.js
@@ -0,0 +1,28 @@
+import { useQuery, useMutation } from 'react-query'
+import { useQueryClient } from 'react-query';
+import axios from 'axios'
+
+const fetchSuperHeroes = () => {
+    return axios.get('http://localhost:4000/superheroes');
+}
+
+const addSuperHero = (hero) => {
+    return axios.post('http://localhost:4000/superheroes', hero);
+}
+
+export const useSuperHeroesData = (onSuccess, onError) => {
+    return useQuery('super-heroes', fetchSuperHeroes, {
+        onSuccess: onSuccess,
+        onError: onError,
+    });
+}
+
+export const useAddSuperHeroData = () => {
+    const queryClient = useQueryClient();
+    return useMutation(addSuperHero, {
+        onMutate: ()=>{},
+        onError: ()=>{},
+        onSettled:()=>{}
+    })
+}
+
