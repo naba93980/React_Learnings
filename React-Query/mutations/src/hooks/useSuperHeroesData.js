ebb9d93ea2665fb32fefe82c3f9ae5e664478c4e
NabajyotiModak <naba93981@gmail.com>
2023-01-24 21:53:27 +0530

    added useAddSuperData() fn tht uses useMutation() hook

--- React-Query/mutations/src/hooks/useSuperHeroesData.js
+++ React-Query/mutations/src/hooks/useSuperHeroesData.js
@@ -1,14 +1,22 @@
-import { useQuery } from 'react-query'
+import { useQuery, useMutation } from 'react-query'
 import axios from 'axios'
 
 const fetchSuperHeroes = () => {
     return axios.get('http://localhost:4000/superheroes');
-    }
+}
 
-const useSuperHeroesData = (onSuccess,onError)=>{
-    return useQuery('super-heroes', fetchSuperHeroes,{
+const addSuperHero = (hero) => {
+    return axios.post('http://localhost:4000/superheroes', hero);
+}
+
+export const useSuperHeroesData = (onSuccess, onError) => {
+    return useQuery('super-heroes', fetchSuperHeroes, {
         onSuccess: onSuccess,
         onError: onError,
-      });
- }
-export default useSuperHeroesData;
+    });
+}
+
+export const useAddSuperHeroData = () => {
+    return useMutation(addSuperHero)
+}
+
